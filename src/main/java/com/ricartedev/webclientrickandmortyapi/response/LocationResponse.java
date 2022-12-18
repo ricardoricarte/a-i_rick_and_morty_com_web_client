aa79cca818184258ed4d97272253d25f45bee8f3
José Ricardo <56279938+ricardoricarte@users.noreply.github.com>
2022-12-18 01:37:19 -0300

    Update LocationResponse.java

--- src/main/java/com/ricartedev/webclientrickandmortyapi/response/LocationResponse.java
+++ src/main/java/com/ricartedev/webclientrickandmortyapi/response/LocationResponse.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import java.util.List;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-
 public class LocationResponse {
 
     private String id;
