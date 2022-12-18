6119e06d71423d17d7015882b954068cd9c2362f
José Ricardo <56279938+ricardoricarte@users.noreply.github.com>
2022-12-18 01:37:37 -0300

    Update ListOfEpisodesResponse.java

--- src/main/java/com/ricartedev/webclientrickandmortyapi/response/ListOfEpisodesResponse.java
+++ src/main/java/com/ricartedev/webclientrickandmortyapi/response/ListOfEpisodesResponse.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import java.util.List;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-
 public class ListOfEpisodesResponse {
 
     List<EpisodeResponse> results;
